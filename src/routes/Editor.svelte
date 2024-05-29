2e347413bfaabd42d59928234c4039649493d11f
poloman719 <poloman7190@gmail.com>
2024-05-28 21:36:05 -0700

    fixed tasks

--- src/routes/Editor.svelte
+++ src/routes/Editor.svelte
@@ -1,17 +1,22 @@
 <script>
+	// @ts-nocheck
+
 	import Task from './Task.svelte';
 	let tasks = [
 		{ text: 'task 1', duration: 5 },
-		{ text: 'task 1', duration: 7 }
+		{ text: 'task 2', duration: 7 }
 	];
-  console.log(tasks)
+	console.log(tasks);
 	let initTime = 5;
+	let taskToAdd = 'type to add task';
 	$: times = [initTime];
 	$: {
 		for (let i = 0; i < tasks.length; i++) {
 			times.push(times[i] + tasks[i].duration);
 		}
 	}
+
+	const onAddTask = () => {};
 </script>
 
 <div class="bg-main-300 w-1/3 p-10 flex flex-col gap-3">
@@ -21,12 +26,13 @@
 		<Task
 			text={task.text}
 			duration={task.duration}
-			time={times[i + 1]}
-      onChange={newText => tasks[i].text = newText}
+			time={task.time}
+			onTimeChange={(e) => (tasks[i].time = e.target.value)}
+			onTextChange={(e) => (tasks[i].text = e.target.value)}
 		/>
 	{/each}
 	<div class="rounded-3xl bg-main-500 p-7 flex justify-between bg-opacity-50">
-		<input type="text" class="outline-none bg-main-500 bg-opacity-0" value="type to add task" />
+		<input type="text" class="outline-none bg-main-500 bg-opacity-0" bind:value={taskToAdd} />
 		<div>15 min</div>
 	</div>
 </div>
--- src/routes/Task.svelte
+++ src/routes/Task.svelte
@@ -2,14 +2,19 @@
 	export let text = '';
 	export let duration = 0;
 	export let time = 0;
-  export let onChange;
-  $: onChange(text);
+	export let onTextChange;
+	export let onTimeChange;
 </script>
 
-<div>
-	<div class="rounded-3xl bg-main-500 p-7 flex justify-between">
-		<input type="text" class="outline-none bg-main-500" bind:value={text} on:change={() => text = "change"}/>
-		<div>{duration} min</div>
+<div class="rounded-3xl bg-main-500 p-7 flex justify-between">
+	<input type="text" class="outline-none bg-main-500" bind:value={text} on:change={onTextChange} />
+	<div>
+		<input
+			type="text"
+			class="outline-none bg-main-500 w-2"
+			bind:value={time}
+			on:change={onTimeChange}
+		/> min
 	</div>
-	<div class="text-center">{time}</div>
 </div>
+<div class="text-center">{time}</div>
